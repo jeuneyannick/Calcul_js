08f29a2cafb309f5a4f2f01e7d9e1b035f635d67
Unknown <yannick.bley@lepoles.com>
2017-08-21 17:02:21 +0200

    slide

--- slider/js/main-jquery.js
+++ slider/js/main-jquery.js
@@ -6,15 +6,24 @@ $(function() {
         var slides = $(this).find(".slide"); // contient la liste des éléments .slide qui sont dans le slider
         var containerWidth = slides.length * 400;
 
+        var isMoving = false; // variable de blocage pour savoir si l'animation est en cours ou non.
+        //Utiliser un booléen pour bloquer un evenement.
+        //Fonctions Call-Back = fonctions appelés quand l'action sur laquelle elle est sera terminée.
 
-        container.css("width", containerWidth)
+
+        container.css("width",containerWidth)
 
         // on ajoute les écouteurs d'evenements pour gérer la navigation.
         $(this).find(".btn-prev").on("click", function(e){
-            e.preventDefault();
+                                      e.preventDefault();
             //alert('backward');
-            if (container.position().left < 0) {
-               container.animate({"left":"+=400"});
+            if (container.position().left < 0 && !isMoving){
+                isMoving = true;// l'animation commence.
+               container.animate({"left":"+=400"},function() {
+                    // fonction de callback de la fonction ANIMATE
+                    //alert('fini')
+                    isMoving = false;// l'animation est terminée
+               });
             }
         });
         $(this).find(".btn-next").on("click", function(e){
